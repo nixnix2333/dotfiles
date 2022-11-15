2bdf67635b84e5a0e9e513a1da26cea3bc5ac75d
nixnix2333 <105246132+nixnix2333@users.noreply.github.com>
2022-11-15 19:54:47 +0800

    feat(nvim): adjust keymaps

--- nvim/lua/user/basic.lua
+++ nvim/lua/user/basic.lua
@@ -25,15 +25,31 @@ vim.g.maplocalleader = " "
 -- lexplore 
 keymap("n", "<leader>e", ":Lex 25<cr>", default_opts)
 
--- tabs
-keymap("n", "<C-n>", ":$tabnew<cr>", default_opts)
-keymap("n", "<C-h>", ":tabprevious<cr>", default_opts)
-keymap("n", "<C-l>", ":tabnext<cr>", default_opts)
+-- windows
+keymap("n", "<C-h>", "<C-w>h", default_opts);
+keymap("n", "<C-j>", "<C-w>j", default_opts);
+keymap("n", "<C-k>", "<C-w>k", default_opts);
+keymap("n", "<C-l>", "<C-w>l", default_opts);
 keymap("n", "<C-c>", "<C-w>c<cr>", default_opts)
 
+-- tabs
+keymap("n", "<C-n>", ":$tabnew<cr>", default_opts)
+keymap("n", "<tab>", ":tabnext<cr>", default_opts)
+
+-- head and tail of a line
+keymap("n", "<S-h>", "^", default_opts)
+keymap("n", "<S-l>", "$", default_opts)
+
 -- select all
-keymap("n", "<leader>a", "ggVG", default_opts)
+keymap("n", "<leader>a", "<esc><esc>ggVG", default_opts)
 
 -- save
 keymap("n", "<C-s>", ":update<cr>", default_opts)
-keymap("i", "<C-s>", "<C-o>:update<cr>", default_opts)
+keymap("i", "<C-s>", "<esc>:update<cr>", default_opts)
+
+-- esc
+keymap("", "<C-f>", "<nop>", default_opts)
+keymap("i", "<C-f>", "<esc>", default_opts)
+
+-- disable join
+keymap("", "<S-j>", "<nop>", default_opts)
