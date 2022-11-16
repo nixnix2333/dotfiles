147722fc634f4a2434945f447606403f3d05877f
nixnix2333 <105246132+nixnix2333@users.noreply.github.com>
2022-11-16 17:52:01 +0800

    feat(nvim): add autopairs package

--- nvim/lua/user/packages.lua
+++ nvim/lua/user/packages.lua
@@ -3,4 +3,10 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  -- autopairs
+  use {
+	  "windwp/nvim-autopairs",
+	  config = function() require("nvim-autopairs").setup {} end
+  }
+
 end)
